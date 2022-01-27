a8d06e03302d857b3a094ea4bec46e8efa45ca7e
Sebastian Carrillo <sebastianulldeveloper@gmail.com>
2022-01-27 10:35:38 -0500

    Feat: Login design implemented

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit
+} from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +15,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit(): void {
+  usernameErrorValidation: boolean = false;
+  // Formulario
+  miFormulario: FormGroup = this.fb.group({
+    name: [, [Validators.required, Validators.maxLength(50)]],
+    username: [, [Validators.required]],
+  });
+
+  constructor(
+    private fb: FormBuilder
+  ) {}
+
+  ngOnInit(): void {}
+
+  async validarUser() {
+
+    if (this.miFormulario.invalid) {
+      this.miFormulario.markAllAsTouched();
+      return;
+    }
+
+    const name = this.miFormulario.controls["name"].value;
+    const username = this.miFormulario.controls["username"].value;
+
+    console.log({name, username})
+
+
+  }
+
+  campoEsValido(campo: string): boolean | null {
+    return this.miFormulario.controls[campo].errors &&
+      this.miFormulario.controls[campo].touched
+  }
+
+  usernameDeleteError(): void {
+    this.usernameErrorValidation = false;
   }
 
 }
